568acbe0c79124fd29797f74195d2aec3fb0e498
sdmwd <simon.durandj@gmail.com>
2023-02-24 17:28:34 +0100

    Update Durand_Simon_3_code_012023.py

--- Durand_Simon_3_code_012023.py
+++ Durand_Simon_3_code_012023.py
@@ -85,7 +85,10 @@ post = st.text_area("Collez ici votre texte :", height=250)
 
 # Si aucun modèle n'est sélectionné, afficher un message d'erreur
 if model_choice is None:
-    st.error("Merci de sélectionner un modèle.")
+    st.error("Merci de sélectionner une approche.")
+
+elif model_choice = " ":
+    st.error("Merci de sélectionner un modèle.")    
 
 else:
 
