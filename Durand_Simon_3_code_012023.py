29654cb5a9a41cc8724f583458b9c833230cd8c3
sdmwd <simon.durandj@gmail.com>
2023-02-24 16:29:20 +0100

    Update Durand_Simon_3_code_012023.py

--- Durand_Simon_3_code_012023.py
+++ Durand_Simon_3_code_012023.py
@@ -66,13 +66,13 @@ st.sidebar.header("Choisir un modèle")
 
 def on_select():
     with st.sidebar.container():
-        unsupervised_choice = st.selectbox("Approche non supervisée", model_functions_unsupervised.keys())
+        unsupervised_choice = st.selectbox("Approche non supervisée", model_functions_unsupervised.keys(), key=3)
 
 with st.sidebar.container():
-    supervised_choice = st.selectbox("Approche supervisée", model_functions_supervised.keys(), on_change=on_select)
+    supervised_choice = st.selectbox("Approche supervisée", model_functions_supervised.keys(), on_change=on_select, key=1)
 
 with st.sidebar.container():
-    unsupervised_choice = st.selectbox("Approche non supervisée", model_functions_unsupervised.keys())
+    unsupervised_choice = st.selectbox("Approche non supervisée", model_functions_unsupervised.keys(), key=2)
 
 
 
