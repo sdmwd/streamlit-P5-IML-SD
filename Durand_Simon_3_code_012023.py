77d5100553d395d761beab3246e39a48ea769693
sdmwd <simon.durandj@gmail.com>
2023-03-04 14:23:39 +0100

    Update Durand_Simon_3_code_012023.py

--- Durand_Simon_3_code_012023.py
+++ Durand_Simon_3_code_012023.py
@@ -72,7 +72,8 @@ with st.sidebar.container():
     if choice == "Approche supervisée":
         with st.sidebar.container():
             model_choice = st.selectbox(" ", model_functions_supervised.keys(),
-                                        format_func=lambda x: f'<span style="color:red">{x}</span>' if x == "LogisticRegression" else x)
+                                        format_func=lambda x: f'<span style="color:red">{x}</span>' if x == "LogisticRegression" else x,
+                                        unsafe_allow_html=True))
 
     if choice == "Approche non supervisée":
         with st.sidebar.container():
